fbdd052d4643af0b0d1f8c912aa6f52fc14a1398
ArleyHL <nhenaol@unal.edu.co>
2025-05-03 20:07:53 -0500

    Create test_migracion.py

---
+++ homework/test/test_migracion.py
@@ -0,0 +1,17 @@
+import os
+
+
+def test_migración():
+
+    if not os.path.exists("data/output/results.tsv"):
+        raise FileNotFoundError("el archivo results.tsv no esiste.")
+    
+    results={}
+    with open("data/output/results.tsv", "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        for line in lines:
+            key, value = line.strip().split("\t")
+            results[key] = value
+
+    assert results.get("computational", 0) == "3"
+    assert results.get("analytics", 0) == "5"
