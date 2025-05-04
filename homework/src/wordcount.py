7ae22cd5677f838f8d2e32fb15133162a286fbe1
ArleyHL <nhenaol@unal.edu.co>
2025-05-03 23:38:36 -0500

    Update wordcount.py

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -4,29 +4,38 @@ import os
 from ._internals.write_count_words import write_count_words
 
 
-def read_all_lines():
+def main():
+
+    ##read all lines
     all_lines = []
     input_file_list = os.listdir("data/input/")
     for filename in input_file_list:
-        with open(filename, "r", encoding="utf-8") as f:
+        file_path = os.path.join("data/input", filename)
+        with open(file_path, "r", encoding="utf-8") as f:
             lines = f.readlines()
             all_lines.extend(lines)
-    return all_lines
 
+    ##preprocess lines
+    all_lines = [line.lower().strip() for line in all_lines]
 
-def main():
+    ##split in words
+    words = []
+    for line in all_lines:
+        words.extend(word.strip(",.!?") for word in line.split())
 
-    input_file_list = os.listdir("data/input/")
-    # all_lines = read_all_lines()
-
+    ##count words
     counter = {}
-    for filename in input_file_list:
-        with open("data/input/" + filename) as f:
-            for l in f:
-                for w in l.split():
-                    w = w.lower().strip(",.!?")
-                    counter[w] = counter.get(w, 0) + 1
+    for word in words:
+        counter[word] = counter.get(word, 0) + 1
 
+    #     counter = {}
+    #     for filename in input_file_list:
+    #         with open("data/input/" + filename) as f:
+    #             for l in f:
+    #                 for w in l.split():
+    #                     w = w.lower().strip(",.!?")
+    #                     counter[w] = counter.get(w, 0) + 1
+    # //##
     write_count_words(counter)
 
 
